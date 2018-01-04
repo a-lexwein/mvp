9a2422a8a74f3be2e5f9d27d9e22a4ea8d21ea72
alex <alexwein@gmail.com>
2018-01-04 12:02:40 -0800

    hello world server and client

---
+++ client/index.jsx
@@ -0,0 +1,13 @@
+const App = () => (
+  <div>
+    <h1>Is it a Sandwich</h1>
+    <h2>An oreo</h2>
+    <button type="button">No</button>
+    <button type="button">Yes</button>
+  </div>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
