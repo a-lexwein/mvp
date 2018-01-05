fee77842d2b52769bfe1e1377c408edceb58d33e
alex <alexwein@gmail.com>
2018-01-04 16:38:35 -0800

    client ajax requests fire with params

--- client/index.jsx
+++ client/index.jsx
@@ -1,16 +1,40 @@
-const App = () => (
-  <div>
-    <h1>Is it a Sandwich</h1>
-    <h2>An oreo</h2>
-    <button type="button">No</button>
-    <button type="button">Yes</button>
-  </div>
-)
+import React from 'react';
+import ReactDom from 'react-dom';
+import $ from 'jquery';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {sandwich: {name:'ham and cheese on rye', id: 0} }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Is it a Sandwich</h1>
+        <h2>{this.state.sandwich.name}</h2>
+        <button type="button" onClick={this.vote.bind(null, this.state.sandwich.id, 'N')}>No</button>
+        <button type="button" onClick={this.vote.bind(null, this.state.sandwich.id, 'Y')}>Yes</button>
+      </div>
+    )
+  }
+
+  getNewSandwich() {
+    $.get('http://127.0.0.1:3000', x => {
+      console.log(x);
+      this.setState({sandwich: JSON.parse(x)})
+    });
+  }
+
+  vote(sandoId, vote) {
+    $.post(`http://127.0.0.1:3000/data/${sandoId}-${vote}`);
+    this.getNewSandwich();
+  }
+
+
+}
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-
-
-$.get('http://127.0.0.1:3000', x => console.log(x))
--- server/index.js
+++ server/index.js
@@ -3,7 +3,13 @@ var app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 
-var sandwiches = ['a carrot','An oreo', 'a hot dog', 'a lobster roll on a hot dog bun', 'lasagna']
+var sandwiches = [
+  {id: 1, name: 'a carrot'},
+  {id: 2, name: 'a hot dog'},
+  {id: 3, name: 'a lobster roll on a hot dog bun'},
+  {id: 4, name: 'lasagna'},
+  {id: 5, name: 'a sausage patty between two waffles'}
+]
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -15,6 +21,11 @@ app.use(function(req, res, next) {
 app.get('/', function (req, res) {
   var sandwich = sandwiches[Math.floor(Math.random() * sandwiches.length)]
   res.send(JSON.stringify(sandwich))
+})
+
+app.post('/data/:sandoId-:bool', function (req, res) {
+  console.log(req.params);
+  res.send(req.params)
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
